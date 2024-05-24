f68cbabcf390f1433576811e3bb351a5e8fe21c2
Youngjin <y98101010@gmail.com>
2024-05-24 16:45:27 +0900

    auth_views revised

--- backend/아맞다/views/auth_views.py
+++ backend/아맞다/views/auth_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template, flash, request, session
+from flask import Blueprint, url_for, render_template, flash, request, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
 
@@ -10,39 +10,40 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/signup/', methods=('GET', 'POST'))
 def signup():
-    data = {
-    "username": "john_doe",
-    "email": "john@example.com",
-    "password1": "password123",
-    "password2": "password123"
-    }
-    form = UserCreateForm(data)
+    data = request.json
+    form = UserCreateForm(data=data)
     if request.method == 'POST' and form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if not user:
-            user = User(username=form.username.data,
-                        password=generate_password_hash(form.password1.data),
-                        email=form.email.data)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('auth.login'))
+        if user:
+            return jsonify({'error': 'User already exists'})
         else:
-            flash('이미 존재하는 사용자입니다.')
-    return render_template('auth/signup.html', form=form)
+            email = User.query.filter_by(email=form.email.data).first()
+            if email:
+                return jsonify({'error': 'Email already exists'})
+            else:                     
+                user = User(username=form.username.data,
+                            password=generate_password_hash(form.password1.data),
+                            email=form.email.data)
+                db.session.add(user)
+                db.session.commit()
+                return jsonify({'success': 'User created successfully'})
+    return jsonify({'error': 'Invalid data or request method'})
 
 @bp.route('/login/', methods=('GET', 'POST'))
 def login():
-    form = UserLoginForm()
+    data = request.json
+    form = UserLoginForm(data=data)
     if request.method == 'POST' and form.validate_on_submit():
         error = None
         user = User.query.filter_by(username=form.username.data).first()
         if not user:
             error = "존재하지 않는 사용자입니다."
+            return jsonify({'error':error})
         elif not check_password_hash(user.password, form.password.data):
             error = "비밀번호가 올바르지 않습니다."
         if error is None:
             session.clear()
             session['user_id'] = user.id
-            return redirect(url_for('main.lost_main'))
-        flash(error)
-    return render_template('auth/login.html', form=form)
+            return jsonify({'success': 'User login successfully'})
+        else:
+            return jsonify({'error':error})
